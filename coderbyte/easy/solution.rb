c80d1ce0f2296f5c26ffc29c6a445c7e31090236
Argonus <argonuspiotr@gmail.com>
2015-09-27 19:21:54 +0200

    tape

---
+++ coderbyte/easy/solution.rb
@@ -0,0 +1,15 @@
+def solution(a)
+diff = [((a[0] - a[1..-1].inject(:+)).abs)]
+    for i in 0..(a.length-1)
+        left = a[0..i].inject(:+)
+        right = a[(i+1)..-1].inject(:+)
+        diff << (left - right).abs
+    end
+    
+    return diff.min
+    
+end
+
+a = [3, 1, 2, 3, 4]
+
+puts solution(a)
